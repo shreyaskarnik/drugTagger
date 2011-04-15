00b42144ea386a73db51db877283af19c5912c14
Neo <mmagusss@gmail.com>
2011-04-15 14:42:59 -0700

    changed as per suggestions by judowill

--- mod_whatizitDrugs.py
+++ mod_whatizitDrugs.py
@@ -6,6 +6,23 @@ import glob
 ### Thanks to JudoWill (Will Dampier) for the help regarding this code.
 
 
+
+def de_safe_xml(kinda_xml):
+    """Converts an escaped HTML/XML into a more normal string."""
+
+    htmlCodes = (
+        ('&', '&amp;'),
+        ('<', '&lt;'),
+        ('>', '&gt;'),
+        ('"', '&quot;'),
+        ("'", '&#39;'))
+
+    for rep, orig in htmlCodes:
+        kinda_xml = kinda_xml.replace(orig, rep)
+    return kinda_xml
+
+
+
 def generate_whatizit_client():
     """Generates a SUDS client for the Whatizit webservice."""
 
@@ -15,25 +32,25 @@ def generate_whatizit_client():
     
 
 
-def ask_whatizit(raw_text, client = None, pipeline = 'whatizitEBIMed'):
+def ask_whatizit(raw_text, client = None, pipeline = 'whatizitDrugs'):
     """A function which queries the Whatizit tool use the SOAP client.
 
-    Care is taken to ensure that identical sentences are not querried 
-    multiple times.
+Care is taken to ensure that identical sentences are not querried
+multiple times.
 
-    Arguments:
-    search_sent_list -- A LIST of sentences to search.
-    client = None -- A SOAP client ... If None then one is created on the fly.
-    pipeline = 'whatizitSwissprot' -- The pipeline to search.
-    """
+Arguments:
+search_sent_list -- A LIST of sentences to search.
+client = None -- A SOAP client ... If None then one is created on the fly.
+pipeline = 'whatizitSwissprot' -- The pipeline to search.
+"""
 
     if client is None:
         client = generate_whatizit_client()
-        (response,tagged_text) = client.service.contact(pipelineName = pipeline, 
-                                        text = raw_text, 
+        (response,tagged_text) = client.service.contact(pipelineName = pipeline,
+                                        text = raw_text,
                                         convertToHtml = False)
         
-        return (response,tagged_text)
+        return (response,de_safe_xml(tagged_text))
     
 
 
@@ -41,16 +58,15 @@ def run_batch (in_path,out_path):
     for infile in glob.glob( os.path.join(in_path, '*.txt') ):
         fname=os.path.basename(infile)
         real_name=os.path.splitext(fname)[0]
-        text_in=open(infile, 'r+')
-        text_raw=text_in.read()
-        text_in.close()
-        (resp,tagged_text)=ask_whatizit(text_raw)
+        with open(infile, 'r+') as text_in:
+            text_raw=text_in.read()
+    
+        (resp,tagged_text)=ask_whatizit(de_safe_xml(text_raw))
         if(resp==200):
             out_xml_name=real_name+'.xml'
             out_file=os.path.join(out_path,out_xml_name)
-            text_out=open(out_file,'w')
-            text_out.write(tagged_text)
-            text_out.close()
+            with open(out_file,'w') as text_out:
+                text_out.write(tagged_text)
         else:
             print "Error in file: " + infile
         
