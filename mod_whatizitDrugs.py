bf1d694c825be5d93cd65da345109905a123077c
Neo <mmagusss@gmail.com>
2011-04-15 16:20:14 -0400

    first commit

---
+++ mod_whatizitDrugs.py
@@ -0,0 +1,56 @@
+# -*- coding: utf8 -*-
+from suds.client import Client
+import re
+import os
+import glob
+### Thanks to JudoWill (Will Dampier) for the help regarding this code.
+
+
+def generate_whatizit_client():
+    """Generates a SUDS client for the Whatizit webservice."""
+
+    url = 'http://www.ebi.ac.uk/webservices/whatizit/ws?wsdl'
+    client = Client(url, faults = False, retxml = False)
+    return client
+    
+
+
+def ask_whatizit(raw_text, client = None, pipeline = 'whatizitEBIMed'):
+    """A function which queries the Whatizit tool use the SOAP client.
+
+    Care is taken to ensure that identical sentences are not querried 
+    multiple times.
+
+    Arguments:
+    search_sent_list -- A LIST of sentences to search.
+    client = None -- A SOAP client ... If None then one is created on the fly.
+    pipeline = 'whatizitSwissprot' -- The pipeline to search.
+    """
+
+    if client is None:
+        client = generate_whatizit_client()
+        (response,tagged_text) = client.service.contact(pipelineName = pipeline, 
+                                        text = raw_text, 
+                                        convertToHtml = False)
+        
+        return (response,tagged_text)
+    
+
+
+def run_batch (in_path,out_path):
+    for infile in glob.glob( os.path.join(in_path, '*.txt') ):
+        fname=os.path.basename(infile)
+        real_name=os.path.splitext(fname)[0]
+        text_in=open(infile, 'r+')
+        text_raw=text_in.read()
+        text_in.close()
+        (resp,tagged_text)=ask_whatizit(text_raw)
+        if(resp==200):
+            out_xml_name=real_name+'.xml'
+            out_file=os.path.join(out_path,out_xml_name)
+            text_out=open(out_file,'w')
+            text_out.write(tagged_text)
+            text_out.close()
+        else:
+            print "Error in file: " + infile
+        
